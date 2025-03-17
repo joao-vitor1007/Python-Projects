3dc8c6c7be23ca1fcdb5ac04492135f2360b0d3c
João Vitor <joaovitorbatista0104@gmail.com>
2025-03-17 20:13:54 -0300

    Update media.py

--- media.py
+++ media.py
@@ -2,6 +2,7 @@ nota1 = float(input('isira a primeira nota: '))
 nota2 = float(input('insira a segunda nota: '))
 nota3 = float(input('insira a terceira nota: '))
 conta = (nota1 + nota2 + nota3)/3
+print(f'A média é: {conta}')
 if conta >= 6:
     print('aprovado')
     
