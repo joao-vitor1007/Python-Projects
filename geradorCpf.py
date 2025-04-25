65e6503d211faaa45bf01f0343ab476768ae4610
João Vitor <joaovitorbatista0104@gmail.com>
2025-04-25 10:51:22 -0300

    Gerador aleatório de CPF válido

--- geradorCpf.py
+++ geradorCpf.py
@@ -1,37 +1,21 @@
 import random
-cpf = input("Digite o CPF sem os números de verificação (somente números): ")
 
-if len(cpf) != 9 or not cpf.isdigit():
-    print("CPF inválido. Digite exatamente 9 números.")
-else:
-    nove_digitos = cpf[:9]
-    multiplicadores = list(range(10, 1, -1))
 
-    soma = 0
-    for i in range(9):
-        soma += int(nove_digitos[i]) * multiplicadores[i]
+nove_digitos = ''.join([str(random.randint(0, 9)) for _ in range(9)])
 
-    resultado = (soma * 10) % 11
-    digito1 = 0 if resultado == 10 else resultado
+multiplicadores1 = list(range(10, 1, -1))
+soma1 = sum(int(nove_digitos[i]) * multiplicadores1[i] for i in range(9))
+resultado1 = (soma1 * 10) % 11
+digito1 = 0 if resultado1 == 10 else resultado1
 
-    print(f'O primeiro dígito de verificação do seu CPF é: {digito1}')
 
-    cpf2 = input('Digite o CPF com o primeiro número de verificação (somente números): ')
-    if len(cpf2) != 10 or not cpf2.isdigit():
-        print('CPF inválido, insira exatamente 10 números.')
-    else:
-        dez_digitos = cpf2[:10]
-        multiplicadores2 = list(range(11, 1, -1))
+dez_digitos = nove_digitos + str(digito1)
+multiplicadores2 = list(range(11, 1, -1))
+soma2 = sum(int(dez_digitos[i]) * multiplicadores2[i] for i in range(10))
+resultado2 = (soma2 * 10) % 11
+digito2 = 0 if resultado2 == 10 else resultado2
 
-        soma2 = 0
-        for i in range(10):
-            soma2 += int(dez_digitos[i]) * multiplicadores2[i]
-        
-        resultado = (soma2 * 10) % 11
-        digito2 = 0 if resultado == 10 else resultado
-        bloco1 = cpf[0:3]
-        bloco2 = cpf[3:6]
-        bloco3 = cpf[6:9]
 
-        print(f'O segundo dígito de verificação do seu CPF é: {digito2}')
-        print(f'Para finalizar, seu cpf completo ficou assim: {bloco1}.{bloco2}.{bloco3}-{digito1}{digito2}')
+cpf_formatado = f"{nove_digitos[:3]}.{nove_digitos[3:6]}.{nove_digitos[6:9]}-{digito1}{digito2}"
+
+print(f"CPF gerado aleatoriamente: {cpf_formatado}")
